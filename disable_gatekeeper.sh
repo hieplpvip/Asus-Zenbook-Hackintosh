b5901231ff1b04274749801df5c9657800968d85
hieplpvip <baohiep2013@gmail.com>
2018-08-03 11:23:25 +0700

    Update script

--- disable_gatekeeper.sh
+++ disable_gatekeeper.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [ "$(id -u)" != "0" ] && [ "$(sudo -n echo 'sudo' 2> /dev/null)" != "sudo" ]; then
+    echo "This script must be run as root!"
+    sudo $0 $@
+    exit 0
+fi
+
 sudo spctl --master-disable
 echo 'GateKeeper disabled'
